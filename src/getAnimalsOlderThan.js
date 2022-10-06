b34d7c992eba14aa8f4eab141926d03a4ffed3e7
brunacarodrigues <brunacris.rodrigues@gmail.com>
2022-10-06 14:59:41 -0300

    requisito 2 e 3

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return data
+    .find((species) => species.name === animal)
+    .residents.every((animAge) => animAge.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,11 +1,10 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  const objVaz = {};
-  
-  if(employeeName == 0) {
-    return objVaz;
-  }
+  if (employeeName === undefined) return {};
+  return employees
+    .find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
