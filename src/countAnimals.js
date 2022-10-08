569b84d5736397fc72c5e83ed1975bd094062537
brunacarodrigues <brunacris.rodrigues@gmail.com>
2022-10-07 21:23:13 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,14 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const animalsResidents = {};
+    species.forEach((specie) => { animalsResidents[specie.name] = specie.residents.length; });
+    return animalsResidents;
+  }
+  return (species.find((specie) => specie.name === animal)).residents.length;
 }
 
+console.log(countAnimals('lions'));
 module.exports = countAnimals;
