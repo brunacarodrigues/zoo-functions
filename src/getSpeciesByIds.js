60e2b0ed94c48a7faca03639354159dbd18aab6a
brunacarodrigues <brunacris.rodrigues@gmail.com>
2022-10-10 20:40:28 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (!ids) {
+    return [];
+  }
+  return ids.map((id) => species.find((specie) => id === specie.id));
 }
 
 module.exports = getSpeciesByIds;
