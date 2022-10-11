b11d48c82a408ef97826e329f2a7904d18a39e54
brunacarodrigues <brunacris.rodrigues@gmail.com>
2022-10-11 12:12:47 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,20 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+  const firstId = employees.find((employer) => employer.id === id)
+    .responsibleFor.find((element) => element);
 
+  const { residents } = species.find((specie) => specie.id === firstId);
+
+  const oldAnimal = residents.reduce((acc, elem) => {
+    if (elem.age > acc.age) {
+      return elem;
+    }
+    return acc;
+  }, residents[0]);
+
+  return [oldAnimal.name, oldAnimal.sex, oldAnimal.age];
+}
+// console.log(getOldestFromFirstSpecies('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 module.exports = getOldestFromFirstSpecies;
