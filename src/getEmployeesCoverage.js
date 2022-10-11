3d694467257437eb22bf5ef44f9f958f63480985
brunacarodrigues <brunacris.rodrigues@gmail.com>
2022-10-11 12:39:14 -0300

    requisito 9

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,46 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function employee(person) {
+  return employees.find((employee01) => employee01.firstName === person.name
+  || employee01.lastName === person.name || employee01.id === person.id);
+}
+
+function specie(person) {
+  return species.filter((specie01) => {
+    if (!person) {
+      throw new Error('Informações inválidas');
+    }
+    return person.responsibleFor.includes(specie01.id);
+  });
+}
+
+function employeesAll() {
+  return employees.map((employee02) => {
+    const arraySpecies = specie(employee02);
+    return {
+      id: employee02.id,
+      fullName: `${employee02.firstName} ${employee02.lastName}`,
+      species: arraySpecies.map((element) => element.name),
+      locations: arraySpecies.map((element) => element.location),
+    };
+  });
+}
+
+function getEmployeesCoverage(person) {
+  if (!person) {
+    return employeesAll();
+  }
+
+  const employeeObject = employee(person);
+  const arraySpecies = specie(employeeObject);
+
+  return {
+    id: employeeObject.id,
+    fullName: `${employeeObject.firstName} ${employeeObject.lastName}`,
+    species: arraySpecies.map((element) => element.name),
+    locations: arraySpecies.map((element) => element.location),
+  };
 }
 
 module.exports = getEmployeesCoverage;
