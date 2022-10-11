2a993eaa8aaa83b2df2e55bf4b64a4500a0bdc9c
brunacarodrigues <brunacris.rodrigues@gmail.com>
2022-10-10 23:00:25 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui - iniciando
+  const child = entrants.filter((person) => person.age < 18).length;
+  const adult = entrants.filter((person) => person.age >= 18 && person.age < 50).length;
+  const senior = entrants.filter((person) => person.age >= 50).length;
+
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.values(entrants).length === 0) {
+    return 0;
+  }
+  const qntdVisits = countEntrants(entrants);
+  const qntdChilds = qntdVisits.child * data.prices.child;
+  const qntdAdults = qntdVisits.adult * data.prices.adult;
+  const qntdSeniors = qntdVisits.senior * data.prices.senior;
+  return qntdChilds + qntdAdults + qntdSeniors;
 }
 
 module.exports = { calculateEntry, countEntrants };
