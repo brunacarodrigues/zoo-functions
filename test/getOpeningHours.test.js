742fcc896ac8516899788a6b1187832d2d042ff5
brunacarodrigues <brunacris.rodrigues@gmail.com>
2022-10-11 10:15:49 -0300

    requisito 16

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -24,4 +24,40 @@ describe('Testes da função getOpeningHours', () => {
   it('Teste se ao passar Wednesday e 9:00 PM, a função irá retornar que o Zoo está fechado', () => {
     expect(getOpeningHours('Wednesday', '09:00-PM')).toBe('The zoo is closed');
   });
+  it('Teste ao passar `Satur` e `10:00-AM` deve lançar uma exceção com a mensagem: `The day must be valid. Exemplo: Tuesday`', () => {
+    function testGetOpeningHours() {
+      getOpeningHours('Satur', '10:00-AM');
+    }
+    expect(testGetOpeningHours).toThrowError('The day must be valid. Example: Monday');
+  });
+  it('Teste se ao não passar números de hora retorna um erro', () => {
+    function testHour() {
+      getOpeningHours('Saturday', 'H9:00-AM');
+    }
+    expect(testHour).toThrowError('The hour should represent a number');
+  });
+  it('Teste ao passar `Sunday` e `15:00-PM` deve lançar uma exceção com a mensagem: `The hour must be between 0 and 12`', () => {
+    function testHourValue() {
+      getOpeningHours('Monday', '15:00-PM');
+    }
+    expect(testHourValue).toThrowError('The hour must be between 0 and 12');
+  });
+  it('Teste se ao passar "Monday" e "09-b0-AM" deve lançar uma exceção com a mensagem: "The minutes should represent a number"', () => {
+    function testMinutes() {
+      getOpeningHours('Monday', '09:b0-AM');
+    }
+    expect(testMinutes).toThrowError('The minutes should represent a number');
+  });
+  it('Teste se ao não passar AM e PM retorna um erro', () => {
+    function testClock() {
+      getOpeningHours('Friday', '09:00-HR');
+    }
+    expect(testClock).toThrowError('The abbreviation must be \'AM\' or \'PM\'');
+  });
+  it('Teste se ao passar `Friday` e `08:60-AM` deve lançar uma exceção com a mensagem: `The minutes must be between 0 and 59`', () => {
+    function testMinutesValue() {
+      getOpeningHours('Friday', '08:60-AM');
+    }
+    expect(testMinutesValue).toThrowError('The minutes must be between 0 and 59');
+  });
 });
